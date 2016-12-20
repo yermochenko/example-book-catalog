71507451520513bbfac54fff699e424c46940667
Sergey Yermochenko <yermochenko@gmail.com>
2016-12-21 01:20:29 +0300

    loading information about books of given author from database

--- www/index.php
+++ www/index.php
@@ -4,6 +4,13 @@
 		$pdo = new PDO(DataBase::DSN, DataBase::USER, DataBase::PASSWORD, array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$authors = $pdo->query('SELECT `id`, `name`, `surname` FROM `author`', PDO::FETCH_ASSOC);
+		if(isset($_GET['id'])) {
+			$books = $pdo->prepare('SELECT `id`, `title`, `pages` FROM `book` WHERE `author_id` = :id');
+			$books->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
+			$books->execute();
+			$books = $books->fetchAll(PDO::FETCH_ASSOC);
+			print_r($books);
+		}
 		$pdo = NULL;
 		include 'view-index.php';
 	} catch(PDOException $e) {
