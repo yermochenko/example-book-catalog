4bc10be64b8c1223b1fc6f1170961475d3505875
Sergey Yermochenko <yermochenko@gmail.com>
2016-12-21 01:29:20 +0300

    loading information about selected author from database

--- www/index.php
+++ www/index.php
@@ -5,6 +5,11 @@
 		$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 		$authors = $pdo->query('SELECT `id`, `name`, `surname` FROM `author`', PDO::FETCH_ASSOC);
 		if(isset($_GET['id'])) {
+			$selectedAuthor = $pdo->prepare('SELECT `id`, `name`, `surname` FROM `author` WHERE `id` = :id');
+			$selectedAuthor->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
+			$selectedAuthor->execute();
+			$selectedAuthor = $selectedAuthor->fetch(PDO::FETCH_ASSOC);
+			print_r($selectedAuthor);
 			$books = $pdo->prepare('SELECT `id`, `title`, `pages` FROM `book` WHERE `author_id` = :id');
 			$books->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
 			$books->execute();
